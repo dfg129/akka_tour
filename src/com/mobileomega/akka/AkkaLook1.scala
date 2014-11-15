94c0725ba0ded7e07e3c821f9556c715744b60f1
dfg129 <dfg129@gmail.com>
2014-11-14 20:50:02 -0500

    init commit

---
+++ src/com/mobileomega/akka/AkkaLook1.scala
@@ -0,0 +1,22 @@
+package com.mobileomega.akka
+
+import akka.actor.{ActorSystem, ActorLogging, Actor, Props}
+
+case object Pint
+
+class Person extends Actor with ActorLogging {
+    def receive = {
+    case Pint => log.info("Thanks for the pint")
+    case _ => log.info("received unknown message")
+  }
+}
+
+object AkkaLook1 extends App {
+  val system = ActorSystem("actor-look-1")
+  
+  val alice = system.actorOf(Props(new Person), "alice")
+  
+  alice ! Pint
+  
+  system.shutdown()
+}
